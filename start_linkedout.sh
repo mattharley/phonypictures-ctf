a73014cfb70b731190f360fb427508de02bd12c3
Matt Harley <matt@mattharley.com>
2015-06-16 11:26:33 +0800

    different ports help

--- start_linkedout.sh
+++ start_linkedout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd /home/hero/Documents/phonypictures-ctf/ && gunicorn 'linkedout:app' -b 0.0.0.0:8000
+cd /home/hero/Documents/phonypictures-ctf/ && gunicorn 'linkedout:app' -b 0.0.0.0:7000
