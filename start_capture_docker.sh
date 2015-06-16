6d8a6c68e18e1a89d9bd46b5d25af95888dbdfed
Matt Harley <matt@mattharley.com>
2015-06-16 18:06:49 +0800

    removed interactive

--- start_capture_docker.sh
+++ start_capture_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -p 5001:5001 -a stdin -a stdout -i -t phony/intranet ./start_capture.sh
+docker run -p 5001:5001 -t phony/intranet ./start_capture.sh
--- start_intranet_docker.sh
+++ start_intranet_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -p 5000:5000 -a stdin -a stdout -i -t phony/intranet ./start_intranet.sh
+docker run -p 5000:5000 -t phony/intranet ./start_intranet.sh
